863351043ded40fe3b516ffde1c106d7b33605e0
Tomas Kuchta - Maly Demon <tomas.kuchta111@gmail.com>
2025-10-13 18:28:08 +0200

    Better init

--- src/init.py
+++ src/init.py
@@ -1,7 +1,12 @@
+from time import sleep
 from .mqtt import mqtt_init
-from .led_hal import init_leds
+from .led_hal import init_leds, set_all_off
 
 def init():
     mqtt_init() ## init mqtt
     init_leds() ## init leds
+    sleep(0.5)
+    set_all_off(5) # Ensure all lights are off at start
+    sleep(1)
     print("Initialization complete")
+    
--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from time import sleep
 from .init import init
 
-from .LightEffects.blink_all import blink_all
 
 def main():
     init() # Initialize the application
-    sleep(0.5)
-    while True:
-     blink_all(500, 3, 1, 10)
 
 
 
